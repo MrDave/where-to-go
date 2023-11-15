a326cb09151dc6b18c4632805e062031a93dcdf7
MrDave <davidschnoll@gmail.com>
2023-11-15 13:05:50 +0200

    Add Image model

--- places/models.py
+++ places/models.py
@@ -10,3 +10,15 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    file = models.ImageField(verbose_name="image file")
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    priority = models.IntegerField()
+
+    class Meta:
+        ordering = ["place", "priority"]
+
+    def __str__(self):
+        return f"{self.priority}. {self.place}"
