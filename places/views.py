48b5b63073ed9eb88ea5ee353daf00a7ae9abbac
MrDave <davidschnoll@gmail.com>
2023-11-15 17:09:38 +0200

    Move data from template to views

--- places/views.py
+++ places/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Place
 
 
 def index(request):
     template = loader.get_template("index.html")
-    context = {}
+
+    places_geojson = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "./static/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "./static/roofs24.json"
+          }
+        }
+      ]
+    }
+
+    context = {"places": places_geojson}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
