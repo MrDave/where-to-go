3a2b265f03336f5b7744b3e59326e85bdf24fba3
MrDave <davidschnoll@gmail.com>
2023-11-15 17:58:57 +0200

    Replace hardcoded places with db entries

--- places/views.py
+++ places/views.py
@@ -7,34 +7,28 @@ from .models import Place
 def index(request):
     template = loader.get_template("index.html")
 
-    places_geojson = {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": "./static/moscow_legends.json"
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "./static/roofs24.json"
-          }
+    places = Place.objects.all()
+
+    features = []
+
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lon, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": "lolkek"
+            }
         }
-      ]
+        features.append(feature)
+
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": features
     }
 
     context = {"places": places_geojson}
