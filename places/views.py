82fd468463ebcaeb90bc6614e1dc39d86dbe9498
MrDave <davidschnoll@gmail.com>
2023-11-16 15:42:00 +0200

    Set up JSON response

--- places/views.py
+++ places/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Place
 from django.shortcuts import get_object_or_404
 
@@ -35,4 +35,14 @@ def index(request):
 
 def place_details(request, place_id):
     place = get_object_or_404(Place, id=place_id)
-    return HttpResponse(str(place.title))
+    response = {
+        "title": place.title,
+        "imgs": [request.build_absolute_uri(image.file.url) for image in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.lon,
+            "lat": place.lat
+        }
+    }
+    return JsonResponse(response, json_dumps_params={'ensure_ascii': False, "indent": 4})
