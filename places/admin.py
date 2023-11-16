62bc805d39ff9dabbeeb07341f7f37ab99a3428b
MrDave <davidschnoll@gmail.com>
2023-11-17 00:51:47 +0200

    Show image thumbnails on Place admin

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import *
+from django.utils.html import format_html
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+
+    def image_thumbnail(self):
+        return format_html(
+            '<img src="{}" style="max-width:200px; max-height:200px"/>'.format(self.file.url)
+        )
+
+    readonly_fields = (image_thumbnail,)
 
 
 @admin.register(Place)
