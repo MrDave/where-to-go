5f4ed1c86ce9eb49bfa01340f5b0ac5d56b4291a
MrDave <davidschnoll@gmail.com>
2023-11-17 12:30:20 +0200

    Implement WYSIWYG editor

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
 
 class Place(models.Model):
     title = models.CharField(max_length=50)
     description_short = models.CharField(verbose_name="short description", max_length=300)
-    description_long = models.TextField(verbose_name="long description")
+    description_long = tinymce_models.HTMLField(verbose_name="long description")
     lon = models.FloatField(verbose_name="longitude")
     lat = models.FloatField(verbose_name="latitude")
 
