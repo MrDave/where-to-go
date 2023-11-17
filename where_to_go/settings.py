dd84efd163fc91ba2bc9c2ef66e739f1a812617e
MrDave <davidschnoll@gmail.com>
2023-11-17 13:40:33 +0200

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = env.str("SECRET_KEY", "REPLACE_ME")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["MrDave.pythonanywhere.com", ".localhost", "127.0.0.1", "[::1]"]
 
 
 # Application definition
@@ -131,6 +131,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = Path(BASE_DIR).joinpath("collected_static")
 
 MEDIA_ROOT = Path(BASE_DIR).joinpath('media')
 
