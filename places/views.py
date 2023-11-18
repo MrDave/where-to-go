4e3c72fc5d7cfbad92765a9ba4571386a1bbe990
MrDave <davidschnoll@gmail.com>
2023-11-18 23:06:48 +0200

    Update views.py

--- places/views.py
+++ places/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from .models import Place
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
