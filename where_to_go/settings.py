ec8a0bde68ead10529f3d39283a90975404c7360
MrDave <davidschnoll@gmail.com>
2023-11-20 16:36:44 +0200

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY", "REPLACE_ME")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", True)
 
-ALLOWED_HOSTS = ["MrDave.pythonanywhere.com", ".localhost", "127.0.0.1", "[::1]"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", [])
 
 
 # Application definition
