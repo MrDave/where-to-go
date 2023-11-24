1bd04475fd3cc63668ecce18fb230884ce45392e
MrDave <davidschnoll@gmail.com>
2023-11-24 22:56:15 +0200

    Separate handle functions into separate functions

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -5,6 +5,37 @@ from django.core.management.base import BaseCommand
 from places.models import Place
 
 
+def fetch_raw_place(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    payload = response.json()
+    return payload
+
+
+def get_or_create_place(payload):
+    place, created = Place.objects.get_or_create(
+        title=payload["title"],
+        lon=payload["coordinates"]["lng"],
+        lat=payload["coordinates"]["lat"],
+        defaults={
+            "short_description": payload["description_short"],
+            "long_description": payload["description_long"],
+        }
+    )
+    return place, created
+
+
+def upload_image(place, img_url, img_number):
+    response = requests.get(img_url)
+    response.raise_for_status()
+
+    file = ContentFile(response.content, name=f"{place} - {img_number}")
+
+    place.images.create(
+        file=file,
+    )
+
+
 class Command(BaseCommand):
     help = "Upload JSON data to db."
 
@@ -18,28 +49,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for url in options["json_urls"]:
-            response = requests.get(url)
-            response.raise_for_status()
-            payload = response.json()
+            payload = fetch_raw_place(url)
 
-            place, created = Place.objects.get_or_create(
-                title=payload["title"],
-                lon=payload["coordinates"]["lng"],
-                lat=payload["coordinates"]["lat"],
-                defaults={
-                    "short_description": payload["description_short"],
-                    "long_description": payload["description_long"],
-                }
-            )
+            place, created = get_or_create_place(payload)
+
+            if not created:
+                continue
+
             for number, img_url in enumerate(payload["imgs"]):
-                response = requests.get(img_url)
-                response.raise_for_status()
-
-                file = ContentFile(response.content, name=f"{place} - {number}")
-
-                place.images.create(
-                    file=file,
-                )
+                upload_image(place, img_url, number)
 
             self.stdout.write(
                 self.style.SUCCESS(f"Successfully created place \"{place}\"")
