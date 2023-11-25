3b7a9d499a0b44da3b792c0c7c5cd9de5280c0c1
MrDave <davidschnoll@gmail.com>
2023-11-25 13:28:41 +0200

    Correct Inline admin

--- places/admin.py
+++ places/admin.py
@@ -9,14 +9,13 @@ class ImageInline(SortableTabularInline):
     model = Image
     ordering = ["priority",]
     extra = 3
+    readonly_fields = ["show_image_thumbnail"]
 
     def show_image_thumbnail(self):
         return format_html(
             "<img src='{}' style='max-width:200px; max-height:200px'/>",
             self.file.url
         )
-
-    readonly_fields = (show_image_thumbnail,)
 
 
 @admin.register(Place)
